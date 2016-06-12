6db48c76195f892e56e4a1de43845ce67d077cb3
Gabe <goregk@guilford.edu>
2016-06-12 18:51:21 -0400

    subprocess Test/move files

--- dataComp/functANDtests/Python_to_R_test.py
+++ dataComp/functANDtests/Python_to_R_test.py
@@ -1,7 +1,20 @@
 #Author:SpaceTuna8
 import dataIO
-
+import subprocess
 # => [1, 2, 3, ... 49, 50]
 data_list = [x for x in range(1,51)]
 
 dataIO.singleCol_CSV("Py_R_test1.csv", "ColumnHeader", data_list)
+str(data_list)
+
+#define command and argument
+command = 'Rsrcipt'
+path2script = 'fromPython.R'
+
+#build subprocess command
+cmd = [command, path2script] + data_list
+
+#check_output will run command and store to result
+x = subprocess.check_output(cmd, univsersal_newlines=True)
+
+print('The maximum of the numbers is:', x)
