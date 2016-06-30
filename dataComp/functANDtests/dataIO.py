1c6d449847e4e00b4ca96bc7de5fa0d5746edbc1
Gabe <goregk@guilford.edu>
2016-06-29 21:36:07 -0400

    Fix count_rowXcols Function

--- dataComp/functANDtests/dataIO.py
+++ dataComp/functANDtests/dataIO.py
@@ -47,7 +47,7 @@ def count_columns_CSV(string_filename):
     ncol=len(next(reader0)) # Read first line and count columns
     return ncol
 
-def count_rowsXcols(string_filename)
+def count_rowsXcols(string_filename):
     result = count_columns_CSV(string_filename) * count_rows_CSV(string_filename)
     return result
 
