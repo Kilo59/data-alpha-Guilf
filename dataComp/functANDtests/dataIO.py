0ed2fa872b0d24bee4facd16b54a5440d94f86a6
Gabe <goregk@guilford.edu>
2016-07-17 13:44:22 -0400

    create & test csv writing functions
    multiCol_CSV: creates .csv file of variable length given a list of
    column lists.
    Uses cols_to_rows_list & list_n
    cols_to_rows_list: takes a list of columns and returns a list of rows
    list_n: returns list of lists with variable length

--- dataComp/functANDtests/dataIO.py
+++ dataComp/functANDtests/dataIO.py
@@ -99,7 +99,56 @@ def csv_list_of_lists(string_filename):
                 col_list.append(row[col_list[0]])
     return list1
 
-####CSV Output
+########|CSV Output|########
+
+'''
+#length of each list must be the same
+def colList_to_rowList(list_of_columns):
+    #lists should have the same length
+    list_of_rows = list_n( len(list_of_columns[0]) )
+
+    list_of_rows[0].append(list_of_columns[0][0])
+    list_of_rows[0].append(list_of_columns[1][0])
+    list_of_rows[0].append(list_of_columns[2][0])
+
+    list_of_rows[1].append(list_of_columns[0][1])
+    list_of_rows[1].append(list_of_columns[1][1])
+    list_of_rows[1].append(list_of_columns[2][1])
+
+    list_of_rows[2].append(list_of_columns[0][2])
+    list_of_rows[2].append(list_of_columns[1][2])
+    list_of_rows[2].append(list_of_columns[2][2])
+
+    list_of_rows[3].append(list_of_columns[0][3])
+    list_of_rows[3].append(list_of_columns[1][3])
+    list_of_rows[3].append(list_of_columns[2][3])
+
+    return list_of_rows
+'''
+#**used in multiCol_CSV function**
+#takes a list of lists (where each list repesents a column of data)
+#sorts it into to a list of list where each list represents a row
+#Example input:     [ [header1, 0.1, 0.10], [header2, 0.2, 0.20] ]
+#Example output:    [ [header1, header2], [0.1, 0.2], [0.10, 0.20] ]
+def cols_to_rows_list(list_of_columns):
+    #lists should have the same length
+    list_of_rows = list_n( len(list_of_columns[0]) )
+    for index, row in enumerate(list_of_rows):
+        for ls_index in range( len(list_of_columns) ):
+            #print(index, ls_index)
+            row.append(list_of_columns[ls_index][index])
+    return list_of_rows
+
+def multiCol_CSV(filename, list_of_columns):
+    row_list = cols_to_rows_list(list_of_columns)
+    print("***Written to CSV file***")
+    with open(filename, 'w', newline='') as csvfile:
+        writer1 = csv.writer(csvfile, delimiter=',')
+        for index, ls in enumerate(row_list):
+            #print(ls)
+            writer1.writerow(ls)
+    return
+
 def singleCol_CSV(string_filename, string_header, item_list):
     with open(string_filename, 'w', newline='') as csvfile:
         writer1 = csv.writer(csvfile, delimiter= ',')
@@ -123,6 +172,11 @@ def doubleCol_CSV(filename, header_list, list_of_columns):
     return
 
 ##########|Misc|#################
+
+#return an empty list of lists of size variable size
+def list_n(size_of_list):
+    ls = [[] for i in range(size_of_list)]
+    return ls
 
 def csv_dialect():
     print(csv.list_dialects)
